62dc4ee630f827bcbd73d28dfc80306832daa85d
Sergiu <58399256+sergiuunity@users.noreply.github.com>
2022-05-10 19:43:53 +0300

    Delete ArtGallery(OOP) directory

--- ArtGallery(OOP)/testing/Testing.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-using namespace std;
--- ArtGallery(OOP)/ui/User_Interface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "User_Interface.h"
-#include "../controller/Repo_Controller.h"
-
-void printMenu();
-
-void start(GalleryController controller);
